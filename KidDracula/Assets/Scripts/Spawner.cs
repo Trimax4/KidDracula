3abf1383f89a18c05a7a2d8bfc192ecf50bbf73a
Brendon <btelliar@uci.edu>
2016-01-15 22:41:35 -0800

    Scrolling and spawners implemented
    New 2D version

---
+++ KidDracula/Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Spawner : MonoBehaviour
+{
+    float buffer = 10.0f;
+    public GameObject gObject;
+    [Range(0f, 3.0f)]
+    public float SpawnTime;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0.5f, SpawnTime);
+    }
+
+    void Spawn()
+    {
+        float rand = Random.Range(0, 3);
+        string spawnerSelected = "Spawner (" + rand + ")";
+        Debug.Log(spawnerSelected);
+
+        Instantiate(gObject, GameObject.Find(spawnerSelected).transform.position, Quaternion.identity);
+    }
+}
---
+++ Scripts/LaserSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LaserSpawner : MonoBehaviour
+{
+    float buffer = 10.0f;
+    public GameObject laser;
+    [Range(0f, 3.0f)]
+    public float SpawnTime;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0.5f, SpawnTime);
+    }
+
+    void Spawn()
+    {
+        float rand = Random.Range(0, 23);
+        string spawnerSelected = "Spawner (" + rand + ")"; 
+
+        Instantiate(laser, GameObject.Find(spawnerSelected).transform.position, Quaternion.identity);
+    }
+}
