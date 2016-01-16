4f5641072e9fc4be7fa4d41119b1c1e394b0e844
warstark <smacha@uci.edu>
2016-01-16 14:36:37 -0800

    movement added

---
+++ KidDracula/Assets/Scripts/Player_movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_movement : MonoBehaviour {
+
+    float speed = 4f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+    }
+}
