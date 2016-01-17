169d18611fe057d2427832339109ecbea8f8a209
warstark <smacha@uci.edu>
2016-01-16 17:05:42 -0800

    collision detection works!!!! HALLELUJAH!!!!!!!!!!

--- KidDracula/Assets/Scripts/Player_movement.cs
+++ KidDracula/Assets/Scripts/Player_movement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Player_movement : MonoBehaviour {
 
     float speed = 4f;
+   
 	// Use this for initialization
 	void Start () {
 	
@@ -12,13 +13,24 @@ public class Player_movement : MonoBehaviour {
 	// Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && (GameObject.Find("Player").transform.position.x > GameObject.Find("Left_boundary").transform.position.x))
         {
             transform.Translate(Vector2.left * speed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else
+        {
+            speed=0f;
+        }
+        speed=4f;
+        if (Input.GetKey(KeyCode.RightArrow)&& (GameObject.Find("Player").transform.position.x < GameObject.Find("Right_boundary").transform.position.x))
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
         }
+        else
+        {
+            speed=0f;
+        }
+        speed=4f;
     }
+  
 }
