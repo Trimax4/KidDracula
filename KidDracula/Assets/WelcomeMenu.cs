94dfd200ea97806e387eddd2b6e6b0e66b883c47
PETEroid <selllfocard@gmail.com>
2016-01-16 18:14:31 -0800

    Add welcome scene

---
+++ KidDracula/Assets/WelcomeMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class WelcomeMenu : MonoBehaviour {
+
+	[SerializeField]
+	public Text endButtonText;
+
+	public void startGame () {
+		SceneManager.LoadScene ("GameScreen");
+	}
+
+	public void EndGame () {
+		endButtonText.text = "No Way...";
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
