8f67ad936985ed9ce8273021dd5160fec00cb618
warstark <smacha@uci.edu>
2016-01-17 22:32:33 -0800

    fix fade in

--- KidDracula/Assets/Scripts/Fading.cs
+++ KidDracula/Assets/Scripts/Fading.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Fading : MonoBehaviour {
 
-	private float alpha;
+    public Texture2D tex;
+	private float alpha = 0f;
 	public float step = 0.15f;
 	public bool isFading = false;
 
@@ -21,14 +22,13 @@ public class Fading : MonoBehaviour {
 			}
 		}
 
-		GUI.depth = -9999999;
+        GUI.depth = -99999;
 		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);
-		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), new RenderTexture (Screen.width, Screen.height, 24));
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), tex);
 	}
 
 	// Use this for initialization
 	void Start () {
-		alpha = 0;
 	}
 	
 	// Update is called once per frame
