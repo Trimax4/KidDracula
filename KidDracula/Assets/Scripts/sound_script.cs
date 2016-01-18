1340bc04c0b4ecd344e709b7835f0931ef8c76b0
warstark <smacha@uci.edu>
2016-01-17 18:05:41 -0800

    sound script

---
+++ KidDracula/Assets/Scripts/sound_script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sound_script : MonoBehaviour {
+    public AudioClip bite;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        if((col.gameObject.name=="Male")||(col.gameObject.name=="Female")||(col.gameObject.name=="Child"))
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+            audio.Play(44100);
+        }
+    }
+}
