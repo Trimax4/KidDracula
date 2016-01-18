51c14108615d6162426da3bc2e13c5438b3539ad
PETEroid <selllfocard@gmail.com>
2016-01-17 21:26:52 -0800

    Add infinte floor

--- KidDracula/Assets/Scripts/Done_BGScroller.cs
+++ KidDracula/Assets/Scripts/Done_BGScroller.cs
@@ -6,17 +6,29 @@ public class Done_BGScroller : MonoBehaviour
 	public float scrollSpeed;
 	public float tileSizeZ;
 	public GameObject repeatTile1, repeatTile2;
-
-	private Vector3 startPosition;
+	private int tileIndex = 0;
+	private Vector3 repeatTile1Position, repeatTile2Position;
 
 	void Start ()
 	{
-		startPosition = transform.position;
+		repeatTile1Position = repeatTile1.transform.localPosition;
+		repeatTile2Position = repeatTile1.transform.localPosition;
 	}
 
 	void Update ()
 	{
 		transform.position += new Vector3(0, -0.1f, 0);
-//		(int) Mathf.Abs (transform.position.y) % transform.localScale.y
+		tileIndex = (int) Mathf.Abs((transform.position.y) / transform.localScale.y);
+		Debug.Log (tileIndex);
+		repeatTile1.transform.localPosition = new Vector3 (
+			0,
+			0 + tileIndex,
+			0
+		);
+		repeatTile2.transform.localPosition = new Vector3 (
+			0,
+			1 + tileIndex,
+			0
+		);		
 	}
 }
