a30b53e4f594aa4fd90e80f46d49144da6ac3328
PETEroid <selllfocard@gmail.com>
2016-01-17 18:36:40 -0800

    Die when no blood

--- KidDracula/Assets/Scripts/Player_movement.cs
+++ KidDracula/Assets/Scripts/Player_movement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class Player_movement : MonoBehaviour {
@@ -8,10 +9,12 @@ public class Player_movement : MonoBehaviour {
 	public GameObject bloodObject;
 	public RectTransform rectTransform;
     private float speed;
+	public Renderer rend;
            
 	// Use this for initialization
 	void Start () {
         speed = resetSpeed;
+		rend = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
@@ -49,5 +52,23 @@ public class Player_movement : MonoBehaviour {
 		}
 		Instantiate (bloodObject, transform.position + offset, Quaternion.Euler (rotation));
 	}
-  
+
+	public void BarelyDie () {
+		rend.material.color = new Color (0.6f, 0.3f, 0.3f, 1);
+		animator.SetTrigger ("PlayerHurt");
+	}
+
+	public void Revive () {
+		rend.material.color = new Color (1, 1, 1, 1);
+		animator.SetTrigger ("PlayerRevive");
+	}
+
+	public void Die () {
+		animator.SetTrigger ("PlayerHurt");
+		Invoke ("LoseGame", 0.5f);
+	}
+
+	private void LoseGame () {
+		SceneManager.LoadScene ("GameOverScreen");
+	}
 }
