93f18dfa9245209a9d6f9cb6bc2b87746140fb7c
PETEroid <selllfocard@gmail.com>
2016-01-17 23:01:50 -0800

    Update buttons

--- KidDracula/Assets/Scripts/GameOverScript.cs
+++ KidDracula/Assets/Scripts/GameOverScript.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GameOverScript : MonoBehaviour {
 
+	[SerializeField]
+	public Text endButtonText;
 	public float delayStart;
+	public int countToLeave;
+	private static string[] sayings = new string[] {
+		"No way...",
+		"R u sure?",
+		"Try again.",
+		"Err 404"
+	};
 
 	public void RestartGame () {
 		SceneManager.LoadScene ("GameScreen");
@@ -22,5 +32,19 @@ public class GameOverScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	private string GetRandomSaying () {
+		Random.seed = ((int) Time.time) % 11;
+		return sayings [Random.Range (0, sayings.Length)];
+	}
+
+	public void EndGame () {
+		if (countToLeave > 0) {
+			endButtonText.text = GetRandomSaying ();
+			countToLeave--;
+		} else {
+			Application.Quit ();
+		}
 	}
 }
--- KidDracula/Assets/Scripts/WelcomeMenu.cs
+++ KidDracula/Assets/Scripts/WelcomeMenu.cs
@@ -8,9 +8,21 @@ public class WelcomeMenu : MonoBehaviour {
 	[SerializeField]
 	public Text endButtonText;
 	public float startDelay;
+	public int countToLeave = 3;
+	private static string[] sayings = new string[] {
+		"No way...",
+		"R u sure?",
+		"Try again.",
+		"Err 404"
+	};
 
 	private void StartGame () {
 		SceneManager.LoadScene ("GameScreen");
+	}
+
+	private string GetRandomSaying () {
+		Random.seed = ((int) Time.time) % 11;
+		return sayings [Random.Range (0, sayings.Length)];
 	}
 
 	public void InvokeToStartGame () {
@@ -18,16 +30,11 @@ public class WelcomeMenu : MonoBehaviour {
 	}
 
 	public void EndGame () {
-		endButtonText.text = "No Way...";
-	}
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		if (countToLeave > 0) {
+			endButtonText.text = GetRandomSaying ();
+			countToLeave--;
+		} else {
+			Application.Quit ();
+		}
 	}
 }
