d99f56b8e23f4d6695b2935a6c93fb42163760c1
PETEroid <selllfocard@gmail.com>
2016-01-17 21:36:46 -0800

    Update the order of people

--- KidDracula/Assets/Scripts/Done_BGScroller.cs
+++ KidDracula/Assets/Scripts/Done_BGScroller.cs
@@ -3,21 +3,18 @@ using System.Collections;
 
 public class Done_BGScroller : MonoBehaviour
 {
-	public float scrollSpeed;
+	public float scrollSpeed = 0.1f;
 	public float tileSizeZ;
 	public GameObject repeatTile1, repeatTile2;
 	private int tileIndex = 0;
-	private Vector3 repeatTile1Position, repeatTile2Position;
 
 	void Start ()
 	{
-		repeatTile1Position = repeatTile1.transform.localPosition;
-		repeatTile2Position = repeatTile1.transform.localPosition;
 	}
 
 	void Update ()
 	{
-		transform.position += new Vector3(0, -0.1f, 0);
+		transform.position += new Vector3(0, scrollSpeed * -1, 0);
 		tileIndex = (int) Mathf.Abs((transform.position.y) / transform.localScale.y);
 		Debug.Log (tileIndex);
 		repeatTile1.transform.localPosition = new Vector3 (
